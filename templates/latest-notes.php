111e0dd9b605ad62f67a2bbf1a76f3118bda42ed
Paul Tibbetts <email@paultibbetts.uk>
2016-07-05 01:44:26 +0100

    adds blockquote styles and ensures latest links/notes code looks similar

--- templates/latest-notes.php
+++ templates/latest-notes.php
@@ -14,18 +14,20 @@ $args = array(
 $posts = new WP_Query( $args );
 
 if ( $posts->have_posts() ) : ?>
-<div class="c-Posts c-Posts--recent">
-	<h2 class="c-Posts-header">Latest Notes</h2>
-	<ul class="o-listOfLinks">
-		<?php while ( $posts->have_posts() ) : $posts->the_post(); ?>
-			<li><?php get_template_part('templates/teaser');?></li>
-		<?php endwhile; ?>
-	</ul>
 
-	<a href="<?= get_post_format_link('aside'); ?>"
-		class="c-Posts-allLink">
-		All Notes
-	</a>
+	<div class="c-Posts c-Posts--recent">
+		<h2 class="c-Posts-header">Latest Notes</h2>
+		<ul class="o-listOfLinks">
+			<?php while ( $posts->have_posts() ) : $posts->the_post(); ?>
+				<li><?php get_template_part('templates/teaser');?></li>
+			<?php endwhile; ?>
+		</ul>
 
-</div>
+		<a href="<?= get_post_format_link('aside'); ?>"
+			class="c-Posts-allLink">
+			All Notes
+		</a>
+
+	</div>
+	
 <?php endif; wp_reset_postdata(); ?>
