64e5cb2e4200f36f7e9dbcfce5add98c6423e704
Paul Tibbetts <email@paultibbetts.uk>
2016-08-08 19:52:35 +0100

    fixes archive display of notes
    last commit on this

--- taxonomy-post_format-post-format-aside.php
+++ taxonomy-post_format-post-format-aside.php
@@ -16,31 +16,11 @@
         if($current_year != $old_date) {
             echo '</ul>';
             echo '<h3 style="text-align:right">'.$current_year.'</h3>';
-            echo '<ul class="o-listOfLinks">';
         }
     }else{
         echo '<h3 style="text-align:right">'.$current_year.'</h3>';
-        echo '<ul class="o-listOfLinks">';
     } ?>
-    <article <?php post_class('h-entry'); ?>>
-      <?php if (get_the_title()): ?>
-        <header>
-          <h1 class="p-name">
-            <?php the_title(); ?>
-          </h1>
-        </header>
-      <?php endif; ?>
-      <div class="e-content">
-        <?php the_content(); ?>
-      </div>
-      <footer>
-        <?php wp_link_pages([
-          'before' => '<nav class="page-nav"><p>' . __('Pages:', 'sage'),
-          'after' => '</p></nav>'
-        ]); ?>
-        <?php get_template_part('templates/entry-meta'); ?>
-      </footer>
-    </article>
+    <?php get_template_part('templates/content-single-aside'); ?>
   <?php endwhile; ?>
   </ul>
 
