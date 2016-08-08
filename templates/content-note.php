595058e4fde85fe15f89a63b702ea2a0352ec9c4
Paul Tibbetts <email@paultibbetts.uk>
2016-08-08 19:29:28 +0100

    changes notes to display content as title if no title & adds link

--- templates/content-note.php
+++ templates/content-note.php
@@ -1,10 +1,20 @@
+<?php
+if (get_the_title() != '') :
+  $title = get_the_title();
+  $display_content = true;
+else:
+  $title = get_the_content();
+  $display_content = false;
+endif;
+?>
+
 <article <?php post_class('h-entry'); ?>>
   <header>
     <h1 class="p-name">
-      <?php the_title(); ?>
+      <?= $title; ?>
     </h1>
   </header>
-  <div class="e-content">
+  <div class="e-content" <?php echo (!$display_content ? 'style="display:none"' : ''); ?>>
     <?php the_content(); ?>
   </div>
   <footer>
@@ -12,6 +22,14 @@
       'before' => '<nav class="page-nav"><p>' . __('Pages:', 'sage'),
       'after' => '</p></nav>'
     ]); ?>
-    <?php get_template_part('templates/entry-meta'); ?>
+    <div class="c-Post-meta" style="margin-bottom:3rem">
+      <a href="<?= get_permalink(); ?>">
+        <span class="c-Post-metaName">Published:</span>
+        <time class="o-timestamp dt-published"
+          datetime="<?= get_post_time('c', true); ?>">
+          <?= get_the_date(); ?> at <?= get_the_time(); ?>
+        </time>
+      </a>
+    </div>
   </footer>
 </article>
