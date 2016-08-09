f7b75ade7cac69799efdfe3b7f79a76256a5a7f3
Paul Tibbetts <email@paultibbetts.uk>
2016-08-09 01:30:27 +0100

    fixes tag display

--- tag.php
+++ tag.php
@@ -4,33 +4,33 @@
   <div class="alert alert-warning">
     <?php _e('Sorry, no results were found.', 'sage'); ?>
   </div>
-  <?php get_search_form(); ?>
-<?php else: ?>
+  <?php get_search_form();
+  else:
 
-  <?php while (have_posts()) : the_post(); ?>
-    <?php
+   while (have_posts()) : the_post();
     $current_year = get_the_date('F Y');
 
-    if( 0 != $wp_query->current_post ) {
+    if( 0 != $wp_query->current_post ) :
         $f = $wp_query->current_post - 1;
         $old_date =   mysql2date( 'F Y', $wp_query->posts[$f]->post_date );
 
-        if($current_year != $old_date) {
-            echo '</ul>';
-            echo '<h3>'.$current_year.'</h3>';
-            echo '<ul class="o-listOfLinks">';
-        }
-    }else{
-        echo '<h3>'.$current_year.'</h3>';
-        echo '<ul class="o-listOfLinks">';
-    } ?>
-      <li>
-        <?php get_template_part('templates/teaser'); ?>
-      </li>
-  <?php endwhile; ?>
-  </ul>
+        if($current_year != $old_date) :
+            echo '<h3 class="u-textRight">'.$current_year.'</h3>';
+        endif;
+    else:
+        echo '<h3 class="u-textRight">'.$current_year.'</h3>';
+    endif;
 
-<?php endif; ?>
+    if (get_post_format() == 'aside'):
+      get_template_part('templates/content-single-aside');
+    elseif(get_post_format() == 'link') :
+      get_template_part('templates/teaser-excerpt');
+    else:
+      get_template_part('templates/teaser');
+    endif;
 
+  endwhile;
 
-<?php the_posts_navigation(); ?>
+endif;
+
+the_posts_navigation();
